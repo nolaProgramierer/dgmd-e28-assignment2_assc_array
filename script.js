7e6f478bf803b1baa6f353409e964f3343c01614
Glenn Langdon <glennlangdon@gmail.com>
2022-02-23 21:56:34 -0600

    working

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-   var prices = [];
-   prices["hotdog"] = 4.5,
-   prices["fries"] = 3.5,
-   prices["soda"] = 1.5;
-   prices["sauerkraut"] = 1;
-   console.log(prices);
-
     document.querySelector('input[type=submit]').addEventListener('click', function (e) {
         let numHotDogs = document.querySelector('input[name=hotdog]').value;
         let numFries = document.querySelector('input[name=fries]').value;
         let numSodas = document.querySelector('input[name=soda]').value;
         let numSks = document.querySelector('input[name=skraut]').value;
+        
         var quantities = getQuantity(numHotDogs, numFries, numSodas, numSks);
         // Add order quantities to array
-        gatherTotals(quantities);
+        var numOfItems = gatherTotals(quantities);
+        var total = totalItems(numOfItems);
 
-        document.querySelector('#order-total').innerHTML = "Your order total is: $" + totalItems(prices).toFixed(2);
+        document.querySelector('#order-total').innerHTML = "Your order total is: $" + total.toFixed(2);
+        // Clear values from form
+        document.querySelector('form').reset();
+        // Prevent page refresh
         e.preventDefault();
     });
 
@@ -30,6 +28,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function gatherTotals(q) {
+        // Associative array for product and price
+        var prices = [];
+        prices["hotdog"] = 4.5,
+        prices["fries"] = 3.5,
+        prices["soda"] = 1.5;
+        prices["sauerkraut"] = 1;
+        console.log(prices);
         var index = 0;
         for (let price in prices) {
             prices[price] = prices[price] * q[index];
